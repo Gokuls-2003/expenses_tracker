e5c0f9a5a2328d9ca549f53c08763c34e4baf8b6
Gokulsenthil <gokulsenthil899@gmail.com>
2023-07-26 23:17:05 +0530

    expenses-item

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,31 @@
+import 'package:expense_tracker/widgets/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget{
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+   return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses>{
+  final List<Expense> _rigesterExpense =[
+    Expense(title: 'Flutter course', amount: 19.50, date: DateTime.now(), category: Category.work),
+     Expense(title: 'cinema', amount: 15, date: DateTime.now(), category: Category.leisure),
+
+
+  ];
+   @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+           const  Text('The chart'),
+         Expanded(child: ExpensesList(expenses: _rigesterExpense))
+        ],
+      ),
+    );  
+  }
+}
