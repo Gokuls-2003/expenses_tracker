24c0322fe46aaa98a02188d8f807feeda8030a01
Gokulsenthil <gokulsenthil899@gmail.com>
2023-07-27 21:11:14 +0530

    dataTimeSet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,31 +1,47 @@
-import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
   @override
   State<Expenses> createState() {
-   return _ExpensesState();
+    return _ExpensesState();
   }
 }
 
-class _ExpensesState extends State<Expenses>{
-  final List<Expense> _rigesterExpense =[
-    Expense(title: 'Flutter course', amount: 19.50, date: DateTime.now(), category: Category.work),
-     Expense(title: 'cinema', amount: 15, date: DateTime.now(), category: Category.leisure),
-
-
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _rigesterExpense = [
+    Expense(
+        title: 'Flutter course',
+        amount: 19.50,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'cinema',
+        amount: 15,
+        date: DateTime.now(),
+        category: Category.leisure),
   ];
-   @override
+
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(context: context, builder: (ctx) => const NewExpense());
+  }
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const   Text('Expense Tracker'),
+        actions: [
+        IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+      ]),
       body: Column(
         children: [
-           const  Text('The chart'),
-         Expanded(child: ExpensesList(expenses: _rigesterExpense))
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _rigesterExpense))
         ],
       ),
-    );  
+    );
   }
 }
