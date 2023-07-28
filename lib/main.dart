0d010f00e86e9d31684b4ea2ce32cf14fe04ab83
Gokulsenthil <gokulsenthil899@gmail.com>
2023-07-28 16:05:37 +0530

    error in dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kcolorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+var kdarkcolorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125));
 void main(){
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(useMaterial3: true,
-      colorScheme: kcolorScheme,
-      appBarTheme: const AppBarTheme().copyWith(
-        backgroundColor: kcolorScheme.onPrimaryContainer,
-        foregroundColor: kcolorScheme.primaryContainer,
-      ),
-      cardTheme:  const CardTheme().copyWith(
-        color: kcolorScheme.secondaryContainer,
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kdarkcolorScheme,
+        cardTheme:  const CardTheme().copyWith(
+        color: kdarkcolorScheme.secondaryContainer,
         margin: const EdgeInsets.symmetric(
           horizontal: 16,
           vertical: 8,
@@ -21,13 +21,23 @@ void main(){
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
           backgroundColor: kcolorScheme.primaryContainer,
+          foregroundColor: kdarkcolorScheme.onPrimaryContainer,
         )
       ),
+      ),
+      theme: ThemeData().copyWith(useMaterial3: true,
+      colorScheme: kcolorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kcolorScheme.onPrimaryContainer,
+        foregroundColor: kcolorScheme.primaryContainer,
+      ),
+      
+      
       textTheme: ThemeData().textTheme.copyWith(
         titleLarge: TextStyle(
-          fontWeight: FontWeight.normal,
+          fontWeight: FontWeight.bold,
           color: kcolorScheme.onSecondaryContainer,
-          fontSize: 20,
+          fontSize: 16  ,
         )
       )
     ),
