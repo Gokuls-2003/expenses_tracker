7f4e233974b1df59fc5aba1f196ad0c5268de42c
Gokulsenthil <gokulsenthil899@gmail.com>
2023-07-28 23:27:59 +0530

    onlyOnLightmode

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,46 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-var kcolorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+var kcolorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 var kdarkcolorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
-  seedColor: Color.fromARGB(255, 5, 99, 125));
+  seedColor: const Color.fromARGB(255, 5, 99, 125));
 void main(){
   runApp(
     MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
-        colorScheme: kdarkcolorScheme,
-        cardTheme:  const CardTheme().copyWith(
-        color: kdarkcolorScheme.secondaryContainer,
-        margin: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 8,
-        )
+      darkTheme: ThemeData.dark().copyWith(useMaterial3: true,
+      colorScheme: kdarkcolorScheme,     
       ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: kcolorScheme.primaryContainer,
-          foregroundColor: kdarkcolorScheme.onPrimaryContainer,
-        )
-      ),
-      ),
-      theme: ThemeData().copyWith(useMaterial3: true,
+      theme: ThemeData().
+      copyWith(useMaterial3: true,
       colorScheme: kcolorScheme,
       appBarTheme: const AppBarTheme().copyWith(
         backgroundColor: kcolorScheme.onPrimaryContainer,
         foregroundColor: kcolorScheme.primaryContainer,
       ),
-      
+      cardTheme:  const CardTheme().copyWith(
+        color: kcolorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 7,
+        )
+      ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kcolorScheme.primaryContainer,
+          foregroundColor: kcolorScheme.onPrimaryContainer,
+        )
+      ),
       
       textTheme: ThemeData().textTheme.copyWith(
         titleLarge: TextStyle(
           fontWeight: FontWeight.bold,
           color: kcolorScheme.onSecondaryContainer,
-          fontSize: 16  ,
+          fontSize: 16,
         )
       )
     ),
+    themeMode: ThemeMode.system,
       home: Expenses(),
     )
   );
